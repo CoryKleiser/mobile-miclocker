5963fffd32e9921fdccaf0e075dbbb35ecf8c0c8
Cory Kleiser <kleisecj@mail.uc.edu>
2016-12-17 18:35:55 -0500

    implement list iteration

--- app/components/add/add.component.ts
+++ app/components/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
+import { ModalDialogParams } from 'nativescript-angular/directives/dialogs';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
     templateUrl: 'add.component.html'
 })
 export class AddComponent {
+
+    constructor(private router: RouterExtensions, private params: ModalDialogParams) {
+        console.log(params.context.msg);
+    }
+
+    public goBack() {
+        this.router.back();
+    }
+
+    public close() {
+        this.params.closeCallback('closed modal');
+    }
 
 }
--- app/components/edit/edit.component.ts
+++ app/components/edit/edit.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/directives/dialogs';
+
+import { AddComponent } from '../add/add.component';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +9,39 @@ import {Component} from '@angular/core';
     templateUrl: 'edit.component.html'
 })
 export class EditComponent {
-    
+
+    public items: any[] = [
+        {
+            text: 'c414',
+            manufacturer: 'AKG',
+            quantity: 1
+        },
+        {
+            text: 'u87',
+            manufacturer: 'neumann',
+            quantity: 2
+        },
+        {
+            text: 'sm57',
+            manufacturer: 'sennheiser',
+            quantity: 5
+        }
+    ];
+
+    constructor(private modal: ModalDialogService, private viewRef: ViewContainerRef) {
+
+    }
+
+    public openModal() {
+        let options: ModalDialogOptions = {
+            context: { msg: 'Hello' },
+            viewContainerRef: this.viewRef
+        };
+        this.modal.showModal(AddComponent, options).then(
+            (result) => {
+                console.log(result);
+            }
+        )
+    }
+
 }
